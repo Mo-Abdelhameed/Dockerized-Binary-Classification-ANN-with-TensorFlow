9a2777b62fbaa9061dc3e29a35d9258a9ce19953
moo3030 <mohamedabdelhamid3030@gmail.com>
2023-08-07 03:36:01 +0300

    test norm

--- src/preprocessing/preprocess.py
+++ src/preprocessing/preprocess.py
@@ -96,7 +96,7 @@ def normalize(input_data: pd.DataFrame, schema: BinaryClassificationSchema, scal
         input_data[numeric_features] = scaler.fit_transform(input_data[numeric_features])
         dump(scaler, paths.SCALER_FILE)
     else:
-        input_data[numeric_features] = scaler.fit_transform(input_data[numeric_features])
+        input_data[numeric_features] = scaler.transform(input_data[numeric_features])
     return input_data
 
 
